e2fbf9a549797f53d7381f92599032d810b27dcb
ildasilva <ildaemanoely@hotmail.com>
2020-03-25 12:24:54 -0300

    feat: creation of routes

--- Aulas/Backend/src/routes.js
+++ Aulas/Backend/src/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+routes.post('/sessions', SessionController.create);
+
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
